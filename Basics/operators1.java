dc8ca62a5c9df8a231b1a30a0900ed660f984555
Aditi <singhaditi.as75@gmail.com>
2026-02-07 23:09:31 +0530

    basic operators

---
+++ Basics/operators1.java
@@ -0,0 +1,11 @@
+public class operators1 {
+    public static void main(String args[]) {
+        int num1 =2;
+        int  result1=num1++ ; //fetch the values and the increment
+        System.out.println(result1); 
+        int result2=++num1; // increment then  fetch the value 
+        System.out.println(result2);
+
+    }
+    
+}
