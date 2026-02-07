292aca2a899743675c72216b9bfff9efb83fc9cc
Aditi <singhaditi.as75@gmail.com>
2026-02-07 23:07:27 +0530

    logical operators

---
+++ Basics/logopr.java
@@ -0,0 +1,13 @@
+public class logopr {
+    public static void main (String[] args){
+        int x=7;
+        int y=5;
+        int a=5;
+        int b=9;
+        boolean  result=x<y && a<b;
+        System.out.println(result);
+        boolean result2=x<y || a<b ;
+        System.out.println(result2);
+    }
+    
+}
