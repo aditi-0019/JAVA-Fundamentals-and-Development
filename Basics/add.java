29b21199e44bdbd34287780ccd11d8e854e05f83
Aditi <singhaditi.as75@gmail.com>
2026-02-07 23:08:44 +0530

    basic addition

---
+++ Basics/add.java
@@ -0,0 +1,13 @@
+public class add {
+    public static void main(String args[]){
+        int num1=3;
+        int num2=5;
+        int result=num1 + num2;
+        //System.out.println(num1+num2);
+        System.out.println(result);
+
+    //   System.out.println(3+5);
+    //   System.out.println(8+7);
+      
+    }
+}
