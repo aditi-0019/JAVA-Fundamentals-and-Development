6e3a81e2a5eec9bc8b0986195b7e6935a0b9fb0f
Aditi <singhaditi.as75@gmail.com>
2026-02-11 15:06:53 +0530

    relational operator

---
+++ Basics/relopr.java
@@ -0,0 +1,14 @@
+public class relopr {
+   public static void main(String [] args) {  
+   int x=6;
+   int y=5;
+
+   boolean result1 =x<y; // false 
+   System.out.println(result1);
+   boolean result2=x>y; //true 
+   System.out.println(result2);
+   boolean result3=x>=y; //true
+   System.out.println(result3);
+
+   }
+}
