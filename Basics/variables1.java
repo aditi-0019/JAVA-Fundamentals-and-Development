e9cf79d1c2ddda208b76682090be485818f39036
Aditi <singhaditi.as75@gmail.com>
2026-02-11 15:07:57 +0530

    variables in jaba

---
+++ Basics/variables1.java
@@ -0,0 +1,25 @@
+public class variables1 {
+    public static void main(String []args){
+        int num1=9;
+        byte by=127;
+        short sh=358;
+        long l=7865;
+
+        float f=5.87f;
+        double d=8.5;
+        char c='a';
+
+        boolean b= 3>5;
+        System.out.println(num1);
+        System.out.println( by);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(sh);
+
+
+    }
+    
+}
