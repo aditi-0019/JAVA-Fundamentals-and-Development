c75e474ad12bad2fae58753279dcac904a37c358
Aditi <singhaditi.as75@gmail.com>
2026-03-03 19:26:59 +0530

    storing negative and floating numbers

---
+++ Basics/numbers.java
@@ -0,0 +1,12 @@
+public class numbers {
+    public static void main(String[] args){
+        float a=8.125f;
+        System.out.printf("%.20f%n",a);  //want the value of floating point number upto 20 deciaml values 8.12500000000000000(upto 20 decimal places )
+        float b=0.7f;
+        System.out.printf("%.20f%n",b); //0.69999998807907100000
+
+
+
+
+    }
+}
