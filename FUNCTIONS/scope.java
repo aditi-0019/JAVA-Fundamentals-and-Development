bd125de9c240f736c082c10888e36e5e39a837e1
Aditi <singhaditi.as75@gmail.com>
2026-03-06 13:33:05 +0530

    scope var

--- FUNCTIONS/scope.java
+++ FUNCTIONS/scope.java
@@ -1,3 +1,25 @@
 public class scope {
-    
+
+   static String name="Aditi";
+    public static void main(String[] args){ 
+        // Scope of a variable 
+
+        int x=4;   //local scope 
+        int y=5;
+
+        if(x==4){
+            int j=7;
+            System.out.println(j);
+        }
+        System.out.println(x+","+y);
+        System.out.println(name);
+        fun(); 
+
+    }
+    static  void fun(){
+        int x=4;
+        int y=5 ;
+        System.out.println(x+","+y);
+        System.out.println(name);
+    }
 }
